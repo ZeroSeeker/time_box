c853b1f2fec2cc15807856e6e23d6c342fd04747
ZeroSeeker <zeroseeker@foxmail.com>
2021-12-08 21:41:20 +0800

    更新version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="time_box",
-    version="0.0.1",
+    version="0.0.2",
     author="ZeroSeeker",
     author_email="zeroseeker@foxmail.com",
     description="Time processing tool collection",
